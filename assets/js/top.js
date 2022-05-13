5eae745d04890cbe127b4bc569882f3d4fa259b9
penpalufo <penpal.pfm@gmail.com>
2022-05-13 20:58:53 +0900

    2022.05.13-2

--- assets/js/top.js
+++ assets/js/top.js
@@ -1,47 +1,37 @@
-/*
- * top.js
- */
-var app = new Vue({
-	el: '#app',
+	/*
+	* top.js
+	*/
+	var app = new Vue({
+		el: '#app',
 
-	/* ==== data ==== */
-	// -- リアクティブなデータを登録
-	data(){
-		return{
-			pagetitle: "CDN版Vue2の雛形",
-			win_width: 0,
-		};
-	},
-
-	/* == mounted == DOMが作成された直後 */
-	mounted(){
-
-		// -- Windowのサイズ変化を監視
-		const _this = this;
-		const resizeObserver = new ResizeObserver(entries => {
-			for (let entry of entries) {
-				_this.win_width = entry.contentRect.width;
-			}
-		});
-		resizeObserver.observe(document.querySelector('body'));
-	},
-
-	/* == watch == 監視 */
-	watch: {
-
-		// -- this.win_widthの値が変化したら
-		win_width(){
-			//alert('リサイズしました幅：' + this.win_width);
+		/* == data == リアクティブなデータを登録 */
+		data(){
+			return{
+				pagetitle: "CDN版Vue2の雛形",
+				win_width: 0,
+			};
 		},
-	},
 
-	/* ==== components ==== */
-	// -- 単一ファイルコンポーネントの登録
-	components: {
-		'my-header': httpVueLoader('./common/components/my-header.vue'),
-		'my-footer': httpVueLoader('./common/components/my-footer.vue'),
-		'my-slick':  httpVueLoader('./assets/components/my-slick.vue'),
-		'my-movies': httpVueLoader('./assets/components/my-movies.vue'),
-	},
+		/* == mounted == DOMが作成された直後 */
+		mounted(){
 
-});
+			// -- Windowのサイズ変化を監視
+			const _this = this;
+			const resizeObserver = new ResizeObserver(entries => {
+				for (let entry of entries) {
+					_this.win_width = entry.contentRect.width;
+				}
+			});
+			resizeObserver.observe(document.querySelector('body'));
+		},
+
+		/* ==== components ==== */
+		// -- 単一ファイルコンポーネントの登録
+		components: {
+			'my-header': httpVueLoader('./common/components/my-header.vue'),
+			'my-footer': httpVueLoader('./common/components/my-footer.vue'),
+			'my-slick':  httpVueLoader('./assets/components/my-slick.vue'),
+			'my-movies': httpVueLoader('./assets/components/my-movies.vue'),
+		},
+
+	});
